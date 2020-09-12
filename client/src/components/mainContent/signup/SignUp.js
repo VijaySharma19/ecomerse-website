1a42f7ec3e028f2786ee9a42000eae7f7d8a344d
VijaySharma19 <vijay29042002@gmail.com>
2020-09-12 23:06:50 +0530

    added validations to signup form
    Signed-off-by: VijaySharma19 <vijay29042002@gmail.com>

--- client/src/components/mainContent/signup/SignUp.js
+++ client/src/components/mainContent/signup/SignUp.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import {Message} from "../alertMessage/Message.js"
+
 import axios from 'axios';
 
 
@@ -14,12 +16,54 @@ export class SignUp extends Component {
             products : '',
             avatar : '',
             password : '',
-            error: ''
+            confirmPassword : '',
+            message: '',
+            showMessage : false
         }
         this.handleChange= this.handleChange.bind(this)
         this.handleSubmit=this.handleSubmit.bind(this)
         this.phonenumberValidate=this.phonenumberValidate.bind(this)
+        this.onHide =this.onHide.bind(this)
+        this.checkPassword = this.checkPassword.bind(this)
+        this.validateUsername =this.validateUsername.bind(this)
     }
+    onHide= ()=>{
+        this.setState({message : '',showMessage:false})
+    }
+
+    validateUsername = ()=>{
+        let regName = /^[a-zA-Z]+ [a-zA-Z]+$/;
+        if(!regName.test(this.state.username)){
+            this.setState({
+                username : '',
+                password : '',
+                confirmPassword : '',
+                message: "Username can only contain a-z or A-Z characters",
+                showMessage : true
+            })
+            return false;
+        }
+        else{
+            return true;
+        }
+
+    }
+
+    checkPassword = ()=>{
+        if(this.state.password===this.state.confirmPassword){
+            return true;
+        }
+        else{
+            this.setState({
+                password : '',
+                confirmPassword : '',
+                message: "Please make sure that your password and confirm password have same value",
+                showMessage : true
+            })
+            return false;
+        }
+    }
+    
     phonenumberValidate(inputtxt)
     {
         let phoneno = /^\d{10}$/;
@@ -29,7 +73,13 @@ export class SignUp extends Component {
         }
         else
         {
-            alert("Please enter 10 digit contact number");
+            this.setState({
+                contactNo: "",
+                password : '',
+                confirmPassword : '',
+                message: "Please enter 10 digit contact number",
+                showMessage : true
+            })
             return false;
         }
     }
@@ -41,7 +91,7 @@ export class SignUp extends Component {
     }
     handleSubmit=(e)=>{
         e.preventDefault()
-        if(this.phonenumberValidate(this.state.contactNo)){
+        if(this.phonenumberValidate(this.state.contactNo)&&this.checkPassword()&&this.validateUsername()){
             axios.post('/users/signup',{
                 username: this.state.username,
                 emailId : this.state.emailId,
@@ -68,57 +118,75 @@ export class SignUp extends Component {
 
     render() {
         return (
-            <div>
-                <h1>Sign Up</h1>
-                <p>{this.state.error}</p>
-                <form onSubmit={this.handleSubmit}>
-                    <label>
-                        Name : 
-                        <input 
-                            required
-                            type="text"
-                            name="username"
-                            value={this.state.username}
-                            placeholder="Enter User Name"
-                            onChange={this.handleChange}
-                        /><br />
-                    </label>
-                    <label>
-                        Email Id : 
-                        <input 
-                            required
-                            type="email"
-                            name="emailId"
-                            placeholder="Enter Email Id"
-                            value={this.state.emailId}
-                            onChange={this.handleChange}
-                        /><br />
-                    </label>
-                    <label>
-                        Contact No : 
-                        <input 
-                            required
-                            type="text"
-                            name="contactNo"
-                            placeholder="Enter Contact Number"
-                            value={this.state.contactNo}
-                            onChange={this.handleChange}
-                        /><br />
-                    </label>
-                    <label>
-                        Password : 
-                        <input 
-                            required
-                            type="password"
-                            name="password"
-                            placeholder="Enter Password"
-                            value={this.state.password}
-                            onChange={this.handleChange}
-                        /><br />
-                    </label>
-                    <button type="submit" >Register</button>
-                </form>
-            </div>
+            <React.Fragment>
+                <div>
+                    <h1>Sign Up</h1>
+                    <p>{this.state.error}</p>
+                    <form onSubmit={this.handleSubmit}>
+                        <label>
+                            Name : 
+                            <input 
+                                required
+                                type="text"
+                                name="username"
+                                value={this.state.username}
+                                placeholder="Enter User Name"
+                                onChange={this.handleChange}
+                            /><br />
+                        </label>
+                        <label>
+                            Email Id : 
+                            <input 
+                                required
+                                type="email"
+                                name="emailId"
+                                placeholder="Enter Email Id"
+                                value={this.state.emailId}
+                                onChange={this.handleChange}
+                            /><br />
+                        </label>
+                        <label>
+                            Contact No : 
+                            <input 
+                                required
+                                type="text"
+                                name="contactNo"
+                                placeholder="Enter Contact Number"
+                                value={this.state.contactNo}
+                                onChange={this.handleChange}
+                            /><br />
+                        </label>
+                        <label>
+                            Password : 
+                            <input 
+                                required
+                                type="password"
+                                name="password"
+                                placeholder="Enter Password"
+                                value={this.state.password}
+                                onChange={this.handleChange}
+                            /><br />
+                        </label>
+                        <label>
+                            Confirm Password : 
+                            <input 
+                                required
+                                type="password"
+                                name="confirmPassword"
+                                placeholder="Confirm your Password"
+                                value={this.state.confirmPassword}
+                                onChange={this.handleChange}
+                            /><br />
+                        </label>
+                        <button type="submit" >Register</button>
+                    </form>
+                </div>
+                <Message
+                    show={this.state.showMessage}
+                    onHide={this.onHide}
+                    message={this.state.message}
+                ></Message>
+            </React.Fragment>
         )
     }
 }
